c8e441e11cddb3dd91fde7bbc355d587b5ca70b2
mazbaul20 <mazbaul20@gmail.com>
2026-06-10 16:06:26 +0600

    Database Connection Create

--- lib/database/database_helper.dart
+++ lib/database/database_helper.dart
@@ -1,3 +1,33 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
+import '../models/todo_model.dart';
+
+class DatabaseHelper {
+  // Singleton pattern
+  static final DatabaseHelper _instance = DatabaseHelper._internal();
+  factory DatabaseHelper() => _instance;
+  DatabaseHelper._internal();
+
+  //Database Variable
+  static Database? _database;
+
+  //Database Getter
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDatabase();
+    return _database!;
+  }//end method
+
+  //Database File Create
+  Future<Database> _initDatabase() async {
+    final directory = await getApplicationDocumentsDirectory();
+    final path = join(directory.path, 'todo_database.db');
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _createTable,
+    );
+  }//end method
+}
